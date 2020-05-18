1673908fb7275a56c260c3d89be20da7288c66d1
Diego Arostegui <darostegui@globalint.info>
2020-05-18 11:42:20 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=darostegui/prediction_service
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=darostegui
 
 # Step 3:
 # Push image to a docker repository
+docker tag ${dockerpath} ${dockerpath}
+docker push ${dockerpath}
