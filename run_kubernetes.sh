d642cdaeea23c77fe1c36d28276c35c6f15b9d98
Diego Arostegui <darostegui@globalint.info>
2020-05-18 11:41:08 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=darostegui/prediction_service
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run prediction-service --image=docker.io/${dockerpath} --port=8000
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployments/prediction-service 8000:80
 
